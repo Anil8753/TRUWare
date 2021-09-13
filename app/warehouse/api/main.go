0ee4c9a825f75409e310d81c631ddc073380d1bf
Anil <anilkumar8753@gmail.com>
2021-09-14 00:45:32 +0530

    Update the API server to serve both API and angular static file

--- app/warehouse/api/main.go
+++ app/warehouse/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
@@ -12,18 +13,25 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.Handle("/", http.FileServer(http.Dir("./www")))
-
+	// First register '/api' routess
 	err := RegisterWarehouseRoutes(r)
 	if err != nil {
 		panic(err)
 	}
 
+	// Second rest other paths serve from 'www' dir
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./www")))
+
 	port := ":8081"
 	fmt.Printf("\nStarting API server at port %s \n", port)
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:8081"},
+		AllowedOrigins: []string{
+			"http://localhost:4200",
+			"http://127.0.0.1:4200",
+			"http://localhost:8081",
+			"http://127.0.0.1:8081",
+		},
 		AllowedMethods: []string{
 			http.MethodHead,
 			http.MethodGet,
@@ -38,7 +46,15 @@ func main() {
 
 	h := c.Handler(r)
 
-	err = http.ListenAndServe(port, h)
+	srv := &http.Server{
+		Handler: h,
+		Addr:    port,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	err = srv.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
